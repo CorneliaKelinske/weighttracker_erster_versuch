ea3866c697901f8f16b03f9005520170a9cfc7e8
CorneliaKelinske <corneliakelinske@gmail.com>
2020-09-06 07:01:00 -0700

    added first template; included a tiny bit of style in the html file

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "FURZKANONE"
+    user = {'username': 'Connie'}
+    return render_template('index.html', title="Home", user=user)

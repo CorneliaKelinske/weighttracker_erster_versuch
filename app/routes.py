585f4655a22f1fe86eba78d11d53d9a9718d36ea
CorneliaKelinske <corneliakelinske@gmail.com>
2020-10-21 11:10:21 -0700

    showing logged in user in template

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Connie'}
+   
     posts = [
         {
             'date' : {'month' : 'May', 'day': 3},
@@ -22,7 +22,7 @@ def index():
         }
 
     ]
-    return render_template('index.html', title="Home", user=user, posts=posts)
+    return render_template('index.html', title="Home", posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
